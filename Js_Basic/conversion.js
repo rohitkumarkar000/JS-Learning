77f6526313cbb57abad1f1a41eb15aacd0e975e5
rohitkumarkar000 <126342324+rohitkumarkar000@users.noreply.github.com>
2024-12-14 16:03:15 +0000

    conversion of a number to string

---
+++ Js_Basic/conversion.js
@@ -0,0 +1,15 @@
+// let value = 5
+// let conversion = (-value)
+// console.log(conversion);
+
+// let str1 = "rohit"
+// let str2 = " V"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log( 2 + 3 * 4 / 5 % 7);
+// console.log(+true);
+// console.log(+false);
+
+
