1814129fdf8186ee2537ff4420cada3a0cc6575a
rohitkumarkar000 <126342324+rohitkumarkar000@users.noreply.github.com>
2024-12-16 17:42:07 +0000

    String

---
+++ Js_Basic/string.js
@@ -0,0 +1,31 @@
+// const myAge = 21
+// const Postion = "Forward"
+
+// console.log(`My Name is Rohit I'm ${myAge} years old I Play ${Postion}`);
+
+// const gameName = new String('Rohit-v')
+
+// console.log(gameName[2]);
+// console.log(gameName.__proto_);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('3'));
+// console.log(gameName.indexOf('t'));
+
+// const NewGameName = gameName.substring(0,2)
+// console.log(NewGameName);
+
+const anotherString = "Rohit-jj-rock"
+// console.log(anotherString.slice(-2,4)); // we can use negative value using Slice  
+
+// const newString = "  Rohit  "
+// console.log(newString);
+// console.log(newString.trim()); // reduces the space
+
+// const url = "https://dfsidngfernd%fjsdmfd"
+
+// console.log((url.replace('df','hhhh'))
+// );
+
+// console.log(url.includes("sid"));
+
+console.log(anotherString.split('-'));
