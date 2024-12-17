cdc7f6210b3ecc95e25d779997beee79e7ef6a00
rohitkumarkar000 <126342324+rohitkumarkar000@users.noreply.github.com>
2024-12-17 10:20:55 +0000

    Array 1

---
+++ Js_Basic/Js_Basic_2/01_array.js
@@ -0,0 +1,48 @@
+// const MyNumber = [6,3,5,4,3,3]
+
+// console.log(MyNumber[3]); 
+
+const myArr2 = new Array(8,7,3,4,5,6)
+// console.log(myArr2[2]);
+
+//Array Methods
+
+// myArr2.push(6)
+// console.log(myArr2);
+
+
+// myArr2.pop()
+// console.log(myArr2);
+
+
+// myArr2.unshift(9)  // Add in the starting of the array
+// console.log(myArr2);
+
+
+// myArr2.shift(9) 
+// console.log(myArr2);
+
+// console.log(myArr2.includes(100));  // Provides the value in true or false only
+
+// console.log(myArr2.indexOf(98)); 
+
+// const newArray = myArr2.join()
+
+// console.log(myArr2);
+// console.log(newArray);  // Join Converts array to string 
+
+// Slice and Splice 
+
+// console.log("A" , myArr2);
+
+// const myn1 = myArr2.slice(1,4) // Slice It includes range // it doesnt manipulate original Array
+
+// console.log(myn1);
+// console.log("B" , myArr2);
+
+// const myn2 = myArr2.splice(2,5) // It Doesnt include range // it does manipulate original Array
+// console.log(myn2)
+// console.log("B" , myArr2);
+
+
+
