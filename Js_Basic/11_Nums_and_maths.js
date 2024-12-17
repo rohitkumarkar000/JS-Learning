f5c8c8e35ca3c83c989acd9e8bce90357291b346
rohitkumarkar000 <126342324+rohitkumarkar000@users.noreply.github.com>
2024-12-17 07:58:58 +0000

    Maths and Number

---
+++ Js_Basic/11_Nums_and_maths.js
@@ -0,0 +1,35 @@
+// const score = 89;
+// console.log(score);
+
+
+// const balance = new Number(90)
+// console.log(balance.toString().length); // Converts Number into string, //.Length is not a method
+// console.log(balance.toString().length.toFixed(2));
+
+// const newNumber = 8998.6734657
+// console.log(newNumber.toPrecision(1));
+
+// const hundreds = 800000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++++++++++ Maths
+
+// console.log(Math.abs(-9)); // Converts Negative Number into Positive 
+// console.log(Math.round(9.89)); // Rounds OFF
+// console.log(Math.ceil(9.89)); // Rounds OFF
+// console.log(Math.floor(9.89)); // Rounds OFF
+// console.log(Math.min(9 , 8 ,9 ,4, 4)); // Min Value Find
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10) + 9);
+
+// const min = 12
+// const max = 15
+
+// console.log(Math.floor((Math.random() * (max - min + 1))));
+
+
+
+
+
+
