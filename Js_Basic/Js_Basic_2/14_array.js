4dea2c4c4dff78f1b42bfe20805b9d167864ee4a
rohitkumarkar000 <126342324+rohitkumarkar000@users.noreply.github.com>
2024-12-17 10:45:33 +0000

    Arrray Basics

---
+++ Js_Basic/Js_Basic_2/14_array.js
@@ -0,0 +1,32 @@
+const heroes = ["IronMan" , "SpiderMan" , "Krrish"]
+const dc_Heroes = ["Batman" , "Groot" , "SuperMan"]
+
+// Heroes.push(Dc_Heroes)
+
+// const allHeroes = heroes.concat(dc_Heroes) // Concat add two Array
+
+// console.log(allHeroes);
+
+// const allNewHeroes = [...heroes, ...dc_Heroes]
+// console.log(allNewHeroes);
+
+// const newArray = [3,3,3,3[4,5,6,7,[43,45,4,6]]] 
+
+// const realNewArray = newArray.flat(Infinity)
+
+// console.log(realNewArray); // Not Solved
+
+// console.log(Array.isArray("Rohit"));
+// console.log(Array.from("Rohit")); //.from Used to converrt string into Array
+
+// console.log(Array.from({name:"Rohit"})); //.from is not capable of convert object into array
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1,score2,score3));
+
+
+
+
