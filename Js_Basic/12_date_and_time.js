fa8670a519bb70d8996c934369c9b774d60b158d
rohitkumarkar000 <126342324+rohitkumarkar000@users.noreply.github.com>
2024-12-17 09:49:14 +0000

    Date and Time

---
+++ Js_Basic/12_date_and_time.js
@@ -0,0 +1,23 @@
+// Date
+
+// let myDate = new Date ()
+// console.log(myDate.toDateString());
+// console.log(myDate.toDateString()); // Provides Date & Day
+// console.log(myDate.toISOString()); // Provides ISO Format
+// console.log(myDate.toLocaleDateString()); // Provides Date Format
+
+// let myCreateDate = new Date("2024-08-05") 
+// // console.log(myCreateDate.toDateString()); //Thu Sep 05 2024
+
+// const timeStamp = Date.now()
+// // console.log(timeStamp);
+// // console.log(myCreateDate.getTime());
+// // console.log(Math.floor(Date.now()/1000));
+// let newDate = new Date
+// console.log(newDate.getMonth() + 1);
+
+newDate.toLocaleString('default' , {
+    weekday : "long"
+})
+
+
