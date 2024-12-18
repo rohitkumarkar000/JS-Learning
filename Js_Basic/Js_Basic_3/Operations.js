678e8b6819324eb00f65e344321ea69936511670
rohitkumarkar000 <126342324+rohitkumarkar000@users.noreply.github.com>
2024-12-18 05:19:43 +0000

    Self Operations

---
+++ Js_Basic/Js_Basic_3/Operations.js
@@ -0,0 +1,37 @@
+// const number1 = 2
+// const number2 = 6
+
+// console.log(number1 + number2);
+
+
+const num1 = 98
+const num2 = 7
+
+// const sum = num1 + num2
+// console.log("this is the sum :", sum);
+
+
+// const multiply = num1*num2
+// console.log("this is the product :", multiply);
+
+// const divide = num1/num2
+// console.log(divide);
+
+// const reminder = num1%num2
+// console.log("this is the reminder :" ,reminder);
+
+// const equal = num1 == num2
+// console.log(equal); //False 
+
+// const comparison = num1 > num2   (TRUE)
+// console.log(comparison);
+
+
+// const comparison = num1 >= num2  (TRUE)
+// console.log(comparison);
+
+
+// const comparison = num1 <= num2  (TRUE)
+// console.log(comparison);
+
+
