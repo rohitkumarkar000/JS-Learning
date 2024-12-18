299373dd75bf5ccf2b7c42ca9d589ababa1a937c
rohitkumarkar000 <126342324+rohitkumarkar000@users.noreply.github.com>
2024-12-18 05:52:27 +0000

    operations (Calculator)

--- Js_Basic/Js_Basic_3/Operations.js
+++ Js_Basic/Js_Basic_3/Operations.js
@@ -1,37 +1,64 @@
-// const number1 = 2
-// const number2 = 6
+// // // const number1 = 2
+// // // const number2 = 6
 
-// console.log(number1 + number2);
-
-
-const num1 = 98
-const num2 = 7
-
-// const sum = num1 + num2
-// console.log("this is the sum :", sum);
+// // // console.log(number1 + number2);
 
 
-// const multiply = num1*num2
-// console.log("this is the product :", multiply);
+// // const num1 = 98
+// // const num2 = 7
 
-// const divide = num1/num2
-// console.log(divide);
-
-// const reminder = num1%num2
-// console.log("this is the reminder :" ,reminder);
-
-// const equal = num1 == num2
-// console.log(equal); //False 
-
-// const comparison = num1 > num2   (TRUE)
-// console.log(comparison);
+// // // const sum = num1 + num2
+// // // console.log("this is the sum :", sum);
 
 
-// const comparison = num1 >= num2  (TRUE)
-// console.log(comparison);
+// // // const multiply = num1*num2
+// // // console.log("this is the product :", multiply);
+
+// // // const divide = num1/num2
+// // // console.log(divide);
+
+// // // const reminder = num1%num2
+// // // console.log("this is the reminder :" ,reminder);
+
+// // // const equal = num1 == num2
+// // // console.log(equal); //False 
+
+// // // const comparison = num1 > num2   (TRUE)
+// // // console.log(comparison);
 
 
-// const comparison = num1 <= num2  (TRUE)
-// console.log(comparison);
+// // // const comparison = num1 >= num2  (TRUE)
+// // // console.log(comparison);
 
 
+// // // const comparison = num1 <= num2  (TRUE)
+// // // console.log(comparison);
+
+
+// Basic Calculator
+
+// function calculator(num1 , num2 , operator){
+
+// switch(operator){
+// case "+" : 
+// return  num1 + num2
+// case "-":
+//     return num1 - num2
+//     case "*" : 
+//     return num1 * num2 
+//     case "/":
+//         return num1 !== 0 / num2 ? num1/num2 : "Can't divide by zero"
+
+//         default :
+//         return "Invalid Operator"
+// }
+// }
+// calculator()
+
+// const num1 = 0;
+// const num2 = 4;
+// const operator = '*';
+
+// const result = calculator(num1 , num2 , operator);
+
+// console.log(`this is  ${result}`);
